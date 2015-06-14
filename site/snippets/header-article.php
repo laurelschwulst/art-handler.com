51c7035d931abc4ff81b4125ee745c5814e60055
Laurel Schwulst <laurel.schwulst@gmail.com>
2015-06-14 17:52:13 -0400

    added scrollbar back in

--- site/snippets/header-article.php
+++ site/snippets/header-article.php
@@ -19,9 +19,9 @@
   echo css($cssURI);
 
   echo js('http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
-  // echo js('assets/script/jquery.mousewheel.js');
-  // echo js('assets/script/jquery.jscrollpane.js');
-  // echo js('assets/script/jquery-scrollto.js');
+  echo js('assets/script/jquery.mousewheel.js');
+  echo js('assets/script/jquery.jscrollpane.js');
+  echo js('assets/script/jquery-scrollto.js');
   echo js('assets/script/script.js');
   echo js('assets/script/lightbox.js');
   ?>

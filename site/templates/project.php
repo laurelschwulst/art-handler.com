a94ca60025a61f8a4414d962d11396ab6cea2578
Laurel Schwulst <laurel.schwulst@gmail.com>
2015-06-18 21:55:51 -0400

    added issue back in

--- site/templates/project.php
+++ site/templates/project.php
@@ -2,21 +2,13 @@
 
   <main class="main article" role="main">
 
-  <!-- <div class="article-container">
-    <div class="text-block">
-      <div class="date-author">
-        <div class="date"><?php echo $page->date("F j, Y") ?></div>
-      </div>
-    </div>
-  </div> -->
-
     <h1><?php echo $page->title()->html() ?></h1>
 
     <div class="article-container">
 
       <div class="text-block">
         <div class="date-author">
-          <!-- <div class="issue">From Issue No. <?php echo $page->issue() ?></div> -->
+          <div class="issue">From Issue No. <?php echo $page->issue() ?></div>
           <div class="date"><?php echo $page->date("F j, Y") ?></div>
           <div class="author">By <?php echo $page->author() ?></div>
         </div>

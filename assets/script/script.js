0b361243ce810a9e040b356c3620c6272d842d44
Matt Lenz <matt@mattlenz.com>
2015-07-01 21:28:38 +1000

    Remove old marquee scripts

--- assets/script/script.js
+++ assets/script/script.js
@@ -28,42 +28,7 @@ $(document).ready(function(){
   var last_small_image = $('figure.small, figure.fifty').last();
   $('<div class="clearer"></div>').insertAfter(last_small_image);
 
-
-  /* Homepage  marquees */
-
-  var width = $(window).width();
-  var height = $(window).height();
-
-  if ($('.marquee').length){
-    $('.marquee').marquee();
-  }
-
-  var randomVerticalOne = Math.floor(Math.random()*height);
-  var randomVerticalTwo = Math.floor(Math.random()*height);
-  var randomVerticalThree = Math.floor(Math.random()*height);
-  var randomVerticalFour = Math.floor(Math.random()*height);
-  var randomVerticalFive = Math.floor(Math.random()*height);
-
-  if (randomVerticalOne < 0 || randomVerticalOne > (height-20)) { randomVerticalOne == 0 }
-  if (randomVerticalTwo < 0 || randomVerticalTwo > (height-20)) { randomVerticalTwo == 0 }
-  if (randomVerticalThree < 0 || randomVerticalThree > (height-20)) { randomVerticalThree == 0 }
-  if (randomVerticalFour < 0 || randomVerticalFour > (height-20)) { randomVerticalFour == 0 }
-  if (randomVerticalFive < 0 || randomVerticalFive > (height-20)) { randomVerticalFive == 0 }
-
-  var randomHorizontalOne = Math.floor(Math.random()*width);
-  var randomHorizontalTwo = Math.floor(Math.random()*width);
-  var randomHorizontalThree = Math.floor(Math.random()*width);
-  var randomHorizontalFour = Math.floor(Math.random()*width);
-  var randomHorizontalFive = Math.floor(Math.random()*width);
-
-  $('.marquee.one').css("top", randomVerticalOne + 72 + "px").css("margin-left", -randomHorizontalOne);
-  $('.marquee.two').css("top", randomVerticalTwo + 72 + "px").css("margin-left", -randomHorizontalTwo);
-  $('.marquee.three').css("top", randomVerticalThree + 72 + "px").css("margin-left", -randomHorizontalThree);
-  $('.marquee.four').css("top", randomVerticalFour + 72 + "px").css("margin-left", -randomHorizontalFour);
-  $('.marquee.five').css("top", randomVerticalFive + 72 + "px").css("margin-left", -randomHorizontalFive);
-
   /* Article scrollbar */
-
 
   /* Article "back to top" link */
   $('#back-to-top').click(function() {

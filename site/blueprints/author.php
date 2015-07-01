bc6f577834c7c3c2e1a7eba38ab2d0b868db318e
Matt Lenz <matt@mattlenz.com>
2015-07-01 17:24:21 +1000

    Add author blueprints

---
+++ site/blueprints/author.php
@@ -0,0 +1,9 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Author
+files: false
+pages: false
+fields:
+  title:
+    label: Title
+    type:  text
---
+++ site/blueprints/authors.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Authors
+pages:
+  template: author
+files: false
+fields:
+  title:
+    label: Title
+    type:  text

e3098911a96b59133e8c3475292a76643c166a59
Matt Lenz <matt@mattlenz.com>
2015-07-01 19:02:23 +1000

    Styles for magazine articles without cover image

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -14,7 +14,7 @@
     ?>
 
     <?php foreach($articles as $project): ?>
-    <a href="<?php echo $project->url() ?>" class="grid-col col-one-quarter">
+    <a href="<?php echo $project->url() ?>" class="grid-col col-one-quarter" data-has-image="<?php echo $project->postimage()->empty() ? 'false' : 'true'; ?>">
       <div class="square"><img src="<?php echo url('assets/images/square-invisible.svg'); ?>"></div>
       <div class="title-container">
         <span class="title"><?php echo $project->title()->html() ?></span>
@@ -42,18 +42,11 @@
 
       </div>
 
-      <?php if($postimage = $project->postimage()->sortBy('sort', 'asc')->first()): ?>
-
-        <?php
-            $filename = $project->postimage();
-            $postimage = $project->files()->find($filename);
-        ?>
-
-        <div class="image">
-          <?php echo thumb($postimage, array('width' => 650)) ?>
-        </div>
-
-      <?php endif ?>
+      <div class="image">
+        <?php if(!$project->postimage()->empty()): ?>
+          <?php echo thumb($project->files()->find($project->postimage()), array('width' => 650)); ?>
+        <?php endif; ?>
+      </div>
 
     </a>
     <?php endforeach ?>

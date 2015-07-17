512f76ef0960c1ffd43a03966c5f8bd08cbdf964
laurelschwulst <laurel.schwulst@gmail.com>
2015-07-17 17:51:56 -0400

    Even more styling updates for mobile

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,11 +1,10 @@
   <footer class="footer cf" role="contentinfo">
 
   <ul id="social" class="mobile">
-  	<a href="#" class="search"><li class="search"></li></a>
-  	<a href="http://instagram.com/arthandlermag/"><li class="instagram"></li></a>
-  	<a href="http://art-handler.tumblr.com"><li class="tumblr"></li></a>
-  	<a href="http://twitter.com/ArtHandlerMag"><li class="twitter"></li></a>
-    <a href="http://www.facebook.com/pages/Art-Handler/"><li class="fb"></li></a>
+  	<a href="http://instagram.com/arthandlermag/" target="_blank"><li class="instagram"></li></a>
+  	<a href="http://art-handler.tumblr.com" target="_blank"><li class="tumblr"></li></a>
+  	<a href="http://twitter.com/ArtHandlerMag" target="_blank"><li class="twitter"></li></a>
+    <a href="http://facebook.com/pages/Art-Handler/735055009910112" target="_blank"><li class="fb"></li></a>
   </ul>
 
   </footer>
